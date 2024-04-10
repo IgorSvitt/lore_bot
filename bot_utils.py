8d15afa2f7e1abc6d63441c8cd27960130425ebf
Han <han029007@gmail.com>
2024-04-10 15:36:26 +0300

    Initial commit

---
+++ bot_utils.py
@@ -0,0 +1,48 @@
+lang_dict = {
+    'fandoms': {'Stray Kids🐺': "Хёнджин/Чанбин",
+                'BTS 💜': "Ким Намджун/Пак Чимин",
+                'Harry Potter🧙‍♂️': "Драко Малфой/Гермиона Грейнджер",
+                'Good Omens (Благие знамения)😇': "Кроули/Азирафель"},
+    'tags': ["Romance💋", "Comedy😂", "Getting together👩‍❤️‍👨"],
+    'ru': {
+        'like': "Нравится 🟢",
+        'dislike': "🔴 Не нравится",
+        'fandoms_message': "Выберите один или несколько фандомов:",
+        'cleared_tags': "Список очищен",
+        'clear': "Очистить мой список",
+        'tags_message': "Выберите один или несколько тегов: \n"
+                        "Их описание можно посмотреть с помощью команды /help",
+        'choose_tag': "Настроить теги для поиска#️⃣",
+        'choose_fandom': "Выбрать фандом⭐️",
+        'activity': "Выберите действие: \n",
+        'form': "Настроить мои предпочтения❤️",
+        'finder': "Перейти к поиску фанфиков🔍",
+        'favorites': "Моя коллекция📚",
+        'back': "Назад",
+        'info': "Узнать больше о LORE ❓",
+        'choose_pairings': "Выбрать пейринги 💑👩‍❤️‍💋‍👩👨‍❤️‍👨"
+    },
+    'en': {
+        'like': "Like 🟢",
+        'dislike': "🔴 Don't like",
+        'fandoms_message': "Select one or more fandoms:",
+        'cleared_tags': "The list has been cleared",
+        'clear': "Clear my list",
+        'tags_message': "Select one or more tags: \n"
+                        "Their description can be viewed using the /help command",
+        'choose_tag': "Set up tags for search#️⃣",
+        'choose_fandom': "Choose a fandom⭐️",
+        'activity': "Choose activity: \n",
+        'form': "Set my preferences❤️",
+        'finder': "Go to fanfiction search🔍",
+        'favorites': "My collection📚",
+        'back': "Back",
+        'info': "Learn more about LORE ❓",
+        'choose_pairings': "Choose pairings💑👩‍❤️‍💋‍👩👨‍❤️‍👨"
+    }
+}
+
+
+def recommend(preferences):
+    cover = 0
+    return cover
